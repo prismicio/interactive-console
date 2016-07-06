aa44d373959eb9fdd984bfe657250213c06414eb
Erwan Loisant <eloisant@gmail.com>
2016-07-06 16:54:58 +0900

    Don't commit typings (except for prismic lib)

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/codemirror/index.d.ts" />
-/// <reference path="globals/history/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/react-codemirror/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
